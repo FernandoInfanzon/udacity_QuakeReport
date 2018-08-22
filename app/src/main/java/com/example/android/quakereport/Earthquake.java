a9b64bf7a8b3016d882988f768f892757e1c697a
Fernando Infanzon <luisfernandoim@gmail.com>
2018-08-22 16:40:52 -0500

    Configuraciones iniciales

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,18 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+
+    private String mMagnitude;
+    private String mLocation;
+    private long mTimeInMilliseconds;
+
+    public Earthquake(String magnitude, String location, long timeInMilliseconds){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+    }
+
+    public String getmMagnitude() {return mMagnitude;}
+    public String getmLocation() {return mLocation;}
+    public long getTimeInMilliseconds() {return mTimeInMilliseconds;}
+}
